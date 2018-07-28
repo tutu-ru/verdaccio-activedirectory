06d145e0128f807dc4666318e7c9a47eb88405f8
Evgeniy Semenov <semenov@tutu.ru>
2018-07-28 11:07:16 +0300

    fix(codestyle): Поправлены отступы

--- index.js
+++ index.js
@@ -5,14 +5,14 @@ var _ = require('lodash');
 var htpasswdPlugin = require('verdaccio-htpasswd');
 
 function Plugin(config, stuff) {
-  var self = Object.create(Plugin.prototype);
-  self._config = config;
-  self._logger = stuff.logger;
-  self._logger.info('Active Directory plugin configuration:\n', config);
-  self._htpasswdPlugin = htpasswdPlugin.default({
-    file: config.extendedUsersFile
-  }, stuff);
-  return self;
+	var self = Object.create(Plugin.prototype);
+	self._config = config;
+	self._logger = stuff.logger;
+	self._logger.info('Active Directory plugin configuration:\n', config);
+	self._htpasswdPlugin = htpasswdPlugin.default({
+		file: config.extendedUsersFile
+	}, stuff);
+	return self;
 }
 
 Plugin.prototype.authenticate = function(user, password, callback) {
@@ -24,22 +24,22 @@ Plugin.prototype.authenticate = function(user, password, callback) {
 		self._logger.warn('Active Directory connection error. Error:', error);
 	});
 
-  /**
-   * Authentication is allowed via AD or htpasswd either
-   */
+	/**
+	 * Authentication is allowed via AD or htpasswd either
+	 */
 	connection.authenticate(username, password, function(err, authenticated) {
 		if (err) {
-      self._logger.warn('Active Directory authentication failed. Error code:', err.code + '.', 'Error:\n', err);
-      if (self._config.extendedUsersFile) {
-        self._logger.info('Trying htpasswd authentication...');
-        self._authenticateViaHtpasswd(user, password, function (hErr, hAuthenticated) {
-          if (hErr) {
-            self._logger.warn('htpasswd authentication failed. Error code:', hErr.code + '.', 'Error:\n', hErr);
-            return callback(hErr);
-          }
-        });
-      }
-      return callback(err);      
+			self._logger.warn('Active Directory authentication failed. Error code:', err.code + '.', 'Error:\n', err);
+			if (self._config.extendedUsersFile) {
+				self._logger.info('Trying htpasswd authentication...');
+				self._authenticateViaHtpasswd(user, password, function (hErr, hAuthenticated) {
+					if (hErr) {
+						self._logger.warn('htpasswd authentication failed. Error code:', hErr.code + '.', 'Error:\n', hErr);
+						return callback(hErr);
+					}
+				});
+			}
+			return callback(err);      
 		}
 
 		if (!authenticated) {
@@ -54,21 +54,21 @@ Plugin.prototype.authenticate = function(user, password, callback) {
 };
 
 Plugin.prototype._authenticateViaHtpasswd = function(user, password, callback) {
-  var username = this._getHtpasswdUsername(user);
-  self._htpasswdPlugin.authenticate(username, password, callback);
+	var username = this._getHtpasswdUsername(user);
+	self._htpasswdPlugin.authenticate(username, password, callback);
 };
 
 Plugin.prototype._adduserViaHtpasswd = function(user, password, callback) {
-  var username = this._getHtpasswdUsername(user);
-  self._htpasswdPlugin.adduser(username, password, callback);
+	var username = this._getHtpasswdUsername(user);
+	self._htpasswdPlugin.adduser(username, password, callback);
 };
 
 Plugin.prototype._getHtpasswdUsername = function(user) {
-  var extendedUsersSuffix = this._config.extendedUsersSuffix;
-  if (!extendedUsersSuffix) {
-    extendedUsersSuffix = 'OUTSOURCE';
-  }
-  return user + '@' + extendedUsersSuffix;
+	var extendedUsersSuffix = this._config.extendedUsersSuffix;
+	if (!extendedUsersSuffix) {
+		extendedUsersSuffix = 'OUTSOURCE';
+	}
+	return user + '@' + extendedUsersSuffix;
 }
 
 /**
@@ -76,15 +76,15 @@ Plugin.prototype._getHtpasswdUsername = function(user) {
  * if the extendedUsersFile paramter is provided in plugin's config
  */
 Plugin.prototype.adduser = function(user, password, callback) {
-  var self = this;
-  
-  if (!this._config.extendedUsersFile) {
-    var message = 'No extendedUsersFile provided in config. Registration is forbidden';
-    self._logger.warn(message);
-    return callback(new Error(message));
-  }
+	var self = this;
+	
+	if (!this._config.extendedUsersFile) {
+		var message = 'No extendedUsersFile provided in config. Registration is forbidden';
+		self._logger.warn(message);
+		return callback(new Error(message));
+	}
 
-  this._adduserViaHtpasswd(user, password, callback);
+	this._adduserViaHtpasswd(user, password, callback);
 };
 
 module.exports = Plugin;
